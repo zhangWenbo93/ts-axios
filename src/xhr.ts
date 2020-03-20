be98faa68d9b98bfbe339a75084aa4161c86c031
wengo <18629531510@163.com>
2020-03-20 16:04:57 +0800

    处理axios url data header

---
+++ src/xhr.ts
@@ -0,0 +1,16 @@
+import { AxiosRequestConfig } from './type/index';
+
+export default function xhr(config: AxiosRequestConfig): void {
+  const { data = null, url, method = 'get', headers } = config
+  const request = new XMLHttpRequest()
+  request.open(method.toUpperCase(), url, true)
+  Object.keys(headers).forEach((name) => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+
+  })
+  request.send(data)
+}
